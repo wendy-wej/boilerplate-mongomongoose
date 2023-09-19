ffb4111f2529a7c7d37e85bc37ad276f69d3a384
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-09-19 13:16:46 +0100

    Use model.find() to Search Your Database

--- myApp.js
+++ myApp.js
@@ -49,7 +49,7 @@ const createAndSavePerson = (done) => {
   })
 };
 
-const arrayOfPeople = [
+var arrayOfPeople = [
   {
     name: "jupefruit",
     age:23,
@@ -77,7 +77,12 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, (err,data) =>{
+    if(err){
+      return done(err)
+    }
+    return done(null, data);
+  })
 };
 
 const findOneByFood = (food, done) => {
